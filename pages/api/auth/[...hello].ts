451ee81fdf2f6bd9c649f663bffbaa4cc797491d
Ben Ford <ben@commando.dev>
2021-01-15 17:54:44 +0000

    fix warning

--- pages/api/auth/[...hello].ts
+++ pages/api/auth/[...hello].ts
@@ -1,4 +1,4 @@
-export default (req, res) => {
+export default (req: { query: { name?: "World" | undefined } }, res: { status: (arg0: number) => { (): any; new(): any; send: { (arg0: string): void; new(): any } } }) => {
     const { name = 'World' } = req.query
     res.status(200).send(`Hello ${name}!`)
 }
